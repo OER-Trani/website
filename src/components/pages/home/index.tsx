7d610d64112fee1f1ee728fcaf1bba03076aee82
Camilla Minutilli <camilla.minutilli@gmail.com>
2024-07-30 16:20:36 +0200

    fix

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,27 +1,28 @@
-"use client"
+'use client';
 //import { useEffect } from 'react';
 
 export default function Home() {
-    /*
+  /*
         useEffect(() => {
             const endpoint = process.env.NEXT_PUBLIC_OER_CMS_ENDPOINT_POSTS;
             const username = process.env.NEXT_PUBLIC_OER_CMS_BA_USER;
             const password = process.env.NEXT_PUBLIC_OER_CMS_BA_PASS;
             const headers = new Headers();
-            headers.append('Authorization', 'Basic' + Buffer.from(username + ":" + password).toString('base64'));
+            headers.append('Authorization', 'Basic' + window.btoa(username + ':' + password));
             const request = new Request(new URL(endpoint), {
-                headers,
-                method: "GET",
-                mode: 'cors'
-            })
-            fetch(request).then(response => {
-                console.log(response?.ok)
+            headers,
+            method: 'GET',
+            mode: 'cors',
+            });
+            fetch(request)
+            .then((response) => {
+                console.log(response?.ok);
                 return response.json();
-            }).catch(e => {
-                debugger
-                console.warn(e)
             })
-        }, []);
-    */
-    return null
+      .catch((e) => {
+        console.warn(e);
+      });
+  }, []);
+*/
+  return null;
 }
