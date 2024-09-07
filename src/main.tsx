00fceba8b52baf07f257719de6389b3ae184f1a0
Camilla Minutilli <camilla.minutilli@gmail.com>
2024-09-07 20:53:33 +0200

    feat: replace nextjs with tanstack libs

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { routeTree } from './routeTree.gen';
+import './index.css';
+
+const queryClient = new QueryClient();
+
+// Set up a Router instance
+const router = createRouter({
+  routeTree,
+  context: {
+    queryClient,
+  },
+  defaultPreload: 'intent',
+  // Since we're using React Query, we don't want loader calls to ever be stale
+  // This will ensure that the loader is always called when the route is preloaded or visited
+  defaultPreloadStaleTime: 0,
+});
+
+// Register things for typesafety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>,
+);
---
+++ src/routes/__root.tsx
@@ -0,0 +1,50 @@
+import { Link, Outlet, createRootRouteWithContext } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import type { QueryClient } from '@tanstack/react-query';
+
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient;
+}>()({
+  component: RootComponent,
+  notFoundComponent: () => {
+    return (
+      <div>
+        <h2>Not found</h2>
+        <p>
+          <Link to="/">Go back to the home page</Link>
+        </p>
+      </div>
+    );
+  },
+});
+
+function RootComponent() {
+  return (
+    <>
+      <nav>
+        <Link
+          to="/"
+          activeProps={{
+            className: 'active',
+          }}
+          activeOptions={{ exact: true }}
+        >
+          Home
+        </Link>
+        {/* <Link
+          to={'/posts'}
+          activeProps={{
+            className: 'active',
+          }}
+        >
+          Posts
+        </Link> */}
+      </nav>
+      <hr />
+      <Outlet />
+      <ReactQueryDevtools buttonPosition="top-right" />
+      <TanStackRouterDevtools position="bottom-right" />
+    </>
+  );
+}
