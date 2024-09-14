557fc76613e9ff8891cb6893fcabc7a455331e20
Camilla Minutilli <camilla.minutilli@gmail.com>
2024-09-14 20:57:18 +0200

    feat: add offset to widget API

--- src/lib/wpapi/features/widgets.ts
+++ src/lib/wpapi/features/widgets.ts
@@ -3,14 +3,14 @@ import wpClient from '../client';
 import { WidgetPostType } from '../types/widgets';
 
 wpClient.widgets = wpClient.registerRoute('wp/v2', '/widgets', {
-  params: ['before', 'after', 'post', 'status'],
+  params: ['before', 'after', 'post', 'status', 'per_page'],
 });
 
 export const widgets = wpClient.widgets() as WPAPI.WPRequest;
 
 export async function getWidgets(widgets: WPAPI.WPRequest): Promise<WidgetPostType[] | null> {
   try {
-    const items = await widgets.status('publish');
+    const items = await widgets.status('publish').per_page('100');
     return items;
   } catch (error) {
     console.error(error);
