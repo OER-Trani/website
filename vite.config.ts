3b8cc3b7bb95963469b794a09c93acd089195953
Camilla Minutilli <camilla.minutilli@gmail.com>
2024-11-01 23:26:12 +0100

    fix: navigation in github pages (#8)
    * fix: valida base path for static assets
    
    * fix: handle navigation on GH Pages
    
    * fix: internal navigation

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(), TanStackRouterVite()],
   test: {
     setupFiles: './vitest.setup.ts',
