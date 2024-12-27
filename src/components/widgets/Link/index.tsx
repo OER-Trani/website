37b2b31830dc2a79ae761237807793aaa449ee99
Camilla Minutilli <camilla.minutilli@gmail.com>
2024-12-27 20:40:10 +0100

    fix: open external link in another tab

--- src/components/widgets/Link/index.tsx
+++ src/components/widgets/Link/index.tsx
@@ -1,5 +1,9 @@
 import { Link } from '../../../lib/wp-rest-api/types/widgets';
 
 export default function LinkWidgetComponent({ text, url }: Link) {
-  return <a href={url}>{text}</a>;
+  return (
+    <a href={url} target="_blank">
+      {text}
+    </a>
+  );
 }
